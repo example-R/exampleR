9343911aa06e38054099b5cd5a739d94b048aca3
example-R <110983285+example-R@users.noreply.github.com>
2022-08-10 14:24:03 +0200

    Add files via upload

---
+++ exampler.js
@@ -0,0 +1,3 @@
+var example = new URLSearchParams(location.search).get("ex");
+var exs = ["prova"]
+var examples = [{"title":"prova","description":"Aixó és una prova","content":"file:///Users/roger/Documents/CODI/HTML/exampler/index.html","use":"Prova-ho!","link":"file:///Users/roger/Documents/CODI/HTML/exampler/index.html#link","download":"file:///Users/roger/Documents/CODI/HTML/exampler/index.html#download"}]
